43bcb1acf5ce1bd4423ad3278b010e3b51232acd
Gerson B Martins <gersonmartins@gmail.com>
2017-08-16 10:28:01 -0300

    split the logic into a lib file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+extern crate greprs;
+
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,37 +16,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In the file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = greprs::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
 
 }
 
-fn run(config: Config) -> Result<(), Box<Error>> {
-let mut f = File::open(config.filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something went wrong reading file");
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
